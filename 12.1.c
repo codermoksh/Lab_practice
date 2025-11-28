ad6b1187b824c2a1c2b1ea34d00a15767b29b036
codermoksh <mokshjain012@gmail.com>
2025-11-28 23:07:12 +0530

    Add files via upload

---
+++ 12.1.c
@@ -0,0 +1,11 @@
+/* Write a program to define some constant variable in preprocessor.*/
+
+#include <stdio.h>
+
+#define NUMBER 10
+
+int main()
+{
+    printf("Number = %d\n", NUMBER);
+    return 0;
+}
